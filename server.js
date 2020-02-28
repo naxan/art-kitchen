69916df1bce329c44cda53a948c028d6ae0653f5
akempel <anna.kb.kempel@gmail.com>
2020-02-28 15:58:55 -0800

    Creates server.js boilerplate

---
+++ server.js
@@ -0,0 +1,31 @@
+// Initializing
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+const PORT = process.env.PORT || 4000;
+
+// database
+// const db = require('./models');
+
+// routes
+// const routes = require('./routes');
+
+// ------ MIDDLEWARE
+
+// serve public assets
+app.use(express.static(__dirname + '/public'));
+
+// use bodyParser
+app.use(bodyParser.json());
+
+// ------ ROUTES
+
+// views
+// app.use('/', routes.views);
+
+// api
+// app.use('/api/v1', routes.api);
+
+// ------ START SERVER
+app.listen(PORT, () => console.log(`Server running at localhost:${PORT}`));
