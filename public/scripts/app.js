421b8ccb94aa9044329b1a43db7ccf827fc97ae2
akempel <anna.kb.kempel@gmail.com>
2020-03-04 09:06:13 -0800

    Creates user signup submission on front page form.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,8 +5,62 @@ let loginSubmit = document.getElementById("login-submit")
 let signupSubmit = document.getElementById("signup-submit")
 let ahaa = document.getElementById("ahaa")
 
+const form = document.getElementById('userForm');
+
 loginBtn.addEventListener("click", () => {
     signupSubmit.textContent = "Log in!"
     signupSubmit.id = "login-submit"
     ahaa.classList = "invisible"
+
+    form.removeEventListener('submit', handleSignupSubmit);
+    form.addEventListener('submit', handleLoginSubmit);
 })
+
+function handleSignupSubmit(e) {
+    let noErrorsFoundYet = true;
+    const userData = {};
+    event.preventDefault();
+
+    // remove feedback
+    document.querySelectorAll('.invalid-feedback').forEach(feedback => feedback.remove());
+
+    const formInputs = [...form.elements];
+    formInputs.forEach((input) => {
+        input.classList.remove('.is-invalid');
+        if (input.type !== 'submit' && input.value === '') {
+            noErrorsFoundYet = false;
+            input.classList.add('is-invalid');
+            input.insertAdjacentHTML('afterend', `
+            <div class="invalid-feedback">
+            Please enter your ${input.name}.
+            </div>
+            `)
+        }
+
+        if (noErrorsFoundYet) {
+            userData[input.name] = input.value;
+        }
+    });
+
+    // submiting data to server if user is valid
+    if (noErrorsFoundYet) {
+        console.log('user data being submitted: ', userData);
+        fetch('/api/v1/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(userData),
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            window.location = '/';
+            alert('Account creation success! Now please log in to continue')
+        })
+        .catch(err => console.log(err));
+    }
+}
+
+// event listeners
+form.addEventListener('submit', handleSignupSubmit);
