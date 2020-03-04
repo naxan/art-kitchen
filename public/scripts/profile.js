257a9aac2b8c8cff2a8b313ec8bb08e65e96dda7
akempel <anna.kb.kempel@gmail.com>
2020-03-04 15:05:26 -0800

    Sets up basework for profile post edit buttons

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -1,10 +1,16 @@
+//import { text } from "body-parser";
+
 // --- CACHED ELEMENTS
 const profileImage = document.getElementById('profile-image');
 const username = document.getElementById('username');
 const bio = document.getElementById('bio');
 const postContainer = document.getElementById('posts');
 
+const deleteBtns = document.querySelectorAll('.delete-btn');
+
 // --- Functions
+
+// Populate page with user info and user posts
 
 // Fetch User Info
 function fetchUser() {
@@ -30,46 +36,106 @@ function render(user) {
     }).join('');
 
     postContainer.insertAdjacentHTML('beforeend', postTemplates);
+
+    // set up event listeners on templated buttons
+    const editBtns = document.querySelectorAll('.edit-btn');
+    editBtns.forEach(editBtn => {
+      editBtn.addEventListener('click', handlePostEdit);
+    });
 }
 
 function postTemplate(post) {
     // if post has an image, create an art post
     if (post.imageUrl) {
         return `
-      <div class="card mb-1 ml-1 art-post">
-              <img src="${post.imageUrl}" class="card-img-top" alt="${post.title}">
-              <div class="card-body">
-                <h5 class="card-title title">${post.title}</h5>
-                <p class="card-text description">${post.description}</p>
-                <div class="row mb-0 tags"> 
-                  <button class="tag btn flora-tag">${post.tags[0]}</button>
-                  <button class="tag btn fauna-tag">${post.tags[1]}</button>
-                  <button class="tag btn mat-tag">${post.tags[2]}</button>
-                  <button class="tag btn mach-tag">${post.tags[3]}</button>
-                </div>
-              </div>
-            </div>
+      <div class="card mb-1 ml-1 art-post" id="${post._id}">
+        <div class="btn-group post-menu">
+        <button type="button" 
+          class="btn btn-secondary dropdown-toggle" 
+          data-toggle="dropdown" aria-haspopup="true" 
+          aria-expanded="false">
+        </button>
+          <div class="dropdown-menu dropdown-menu-right">
+            <button class="dropdown-item edit-btn" type="button" for="edit">Edit</button>
+            <button class="dropdown-item delete-btn" type="button" for="delete">Delete</button>
+          </div>
+        </div>
+        <img src="${post.imageUrl}" class="card-img-top" alt="${post.title}">
+        <div class="card-body">
+          <h5 class="card-title title">${post.title}</h5>
+          <p class="card-text description">${post.description}</p>
+          <div class="row mb-0 tags"> 
+            <button class="tag btn flora-tag">${post.tags[0]}</button>
+            <button class="tag btn fauna-tag">${post.tags[1]}</button>
+            <button class="tag btn mat-tag">${post.tags[2]}</button>
+            <button class="tag btn mach-tag">${post.tags[3]}</button>
+          </div>
+        </div>
+      </div>
         `;
     } 
     // if post does NOT have image, create a text post
     else {
         return `
-      <div class="card mb-1 ml-1 text-post">
-      <div class="card-body">
-        <h5 class="card-title title" >${post.title}</h5>
-        <p class="card-text body">${post.body}</p>
-          <p class="text-muted">About this piece:</p>
-        <p class="text-muted description">${post.description}</p>
-        <div class="row mb-0 tags"> 
-            <button class="tag btn flora-tag">${post.tags[0]}</button>
-            <button class="tag btn fauna-tag">${post.tags[1]}</button>
-            <button class="tag btn mat-tag">${post.tags[2]}</button>
-            <button class="tag btn mach-tag">${post.tags[3]}</button>
+      <div class="card mb-1 ml-1 text-post" id="${post._id}">
+        <div class="btn-group post-menu">
+        <button type="button" 
+          class="btn btn-secondary dropdown-toggle" 
+          data-toggle="dropdown" aria-haspopup="true" 
+          aria-expanded="false">
+        </button>
+          <div class="dropdown-menu dropdown-menu-right">
+            <button class="dropdown-item edit-btn" type="button" for="edit">Edit</button>
+            <button class="dropdown-item delete-btn" type="button" for="delete">Delete</button>
+          </div>
+        </div>
+        <div class="card-body">
+          <h5 class="card-title title">${post.title}</h5>
+          <p class="card-text body">${post.body}</p>
+            <p class="text-muted">About this piece:</p>
+          <p class="text-muted description">${post.description}</p>
+          <div class="row mb-0 tags"> 
+              <button class="tag btn flora-tag">${post.tags[0]}</button>
+              <button class="tag btn fauna-tag">${post.tags[1]}</button>
+              <button class="tag btn mat-tag">${post.tags[2]}</button>
+              <button class="tag btn mach-tag">${post.tags[3]}</button>
+          </div>
         </div>
       </div>
-    </div>
         `;
     }
+}
+
+// Edit and Delete posts
+
+// let postId = target.closest('.card').id
+
+// when edit button is clicked,
+// post content changes to form
+// target.closest('.card').innerHTML = formTemplate
+// make function updatePost
+// formIsValid = false;
+// take id's of modal inputs
+// if both values are empty strings,
+// formIsValid = false, parentnode insert adjacent html of an error message, return
+// else,
+// formIsValid = true;
+// if formIsValid
+// const updatedPost = {title: title.value, description: description.value}
+// fetch ('/api/v1/posts/${postId}', {
+  // method: 'PUT',
+  // etc etc
+// })
+// then close modal
+// then remove all posts from DOM
+// then call fetchUser()
+
+// fetch update route
+// body set equal to updatedPost = {title: title.value, description: description.value}
+
+function handlePostEdit(e) {
+  const postId = event.target.closest('.card').id;
+  console.log(postId);
 }
 
 // --- CALLED FUNCTIONS
